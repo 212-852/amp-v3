675767d3b03d79be49ad3a4f779a98d15b23a6f1
Maki  Okino <m.okino@MakinoMacBook-Pro.local>
2026-08-13 19:19:44 +0900

    Update favicon to pink paw icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
+        url: "/icons/icon.svg",
+        sizes: "any",
+        type: "image/svg+xml",
+      },
+      {
         url: "/icons/icon_192.png",
         sizes: "192x192",
         type: "image/png",
